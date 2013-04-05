7f50791ec89bb4a6b3a7d345b54cadd8ff7ed8e8
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-05 01:50:33 -0700

    add new links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 MusicApp::Application.routes.draw do
-  resources :albums
-  resources :bands
+  resources :albums do
+    resources :tracks, :only => [:new]
+  end
+  resources :bands do
+    resources :albums, :only => [:new]
+  end
   resources :tracks
 
   root :to => "bands#index"
