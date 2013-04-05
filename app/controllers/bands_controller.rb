af93625717ebf6e1012a53856cf74eee740beaf4
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-05 01:14:58 -0700

    add new band form

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -6,4 +6,18 @@ class BandsController < ApplicationController
   def show
     @band = Band.find(params[:id])
   end
+  
+  def new
+    @band = Band.new
+  end
+  
+  def create
+    @band = Band.new(params[:band])
+    
+    if @band.save
+      redirect_to band_url(@band)
+    else
+      render :new
+    end
+  end
 end
