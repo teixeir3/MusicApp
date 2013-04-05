4ba8c2fea14fc9261c2fe794cbefd43fa4608d21
Ned Ruggeri <ruggeri@self-loop.com>
2013-04-05 01:32:37 -0700

    add track form

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,20 @@
 class TracksController < ApplicationController
+  def create
+    @track = Track.new(params[:track])
+    
+    if @track.save
+      redirect_to track_url(@track)
+    else
+      render :new
+    end
+  end
+  
   def index
     @tracks = Track.all
+  end
+  
+  def new
+    @track = Track.new
   end
   
   def show
