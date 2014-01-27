73143cbfee503e835de9ee6b974423a4d067c2ca
Tommy Duek <taduek@gmail.com>
2014-01-27 16:10:03 -0500

    Add ActiveRecord::Base#generate_unique_token_for_field and set User#set_activation_token before create.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+class ActiveRecord::Base
+  def self.generate_unique_token_for_field(field)
+    begin
+      token = SecureRandom.base64(16)
+    end until !self.exists?(field => token)
+
+    token
+  end
+end
+
 class User < ActiveRecord::Base
   attr_accessible :email, :password
 
@@ -9,16 +19,21 @@ class User < ActiveRecord::Base
             :presence => true
             )
 
-#  validates(:email, :uniqueness => true)
+  before_create :set_activation_token
 
+#  validates(:email, :uniqueness => true)
   def self.generate_session_token
-    SecureRandom.urlsafe_base64(16)
+    self.class.generate_unique_token_for_field(:session_token)
   end
 
   def self.find_by_credentials(email, secret)
     user = User.find_by_email(email)
 
     user.is_password?(secret) ? user : nil
+  end
+
+  def set_activation_token
+    self.activation_token = self.class.generate_unique_token_for_field(:activation_token)
   end
 
   def password=(secret)
